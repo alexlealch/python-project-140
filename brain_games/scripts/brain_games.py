1f9d6334427f78709b851be5007e15ada2b44093
alexlealch <alexlealch@gmail.com>
2026-02-18 12:51:12 -0500

    juego 1 completado

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,11 @@
 from brain_games.cli import welcome_user
+from brain_games.scripts.brain_even import is_even_or_odd
 
 
 def main():
     print("Welcome to the Brain Games!")
     welcome_user()
+    is_even_or_odd()
 
 
 if __name__ == "__main__":
