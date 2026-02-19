471f6a6c4cf6f897b710147e8486c2cc8433aaf7
alexlealch <alexlealch@gmail.com>
2026-02-19 09:23:00 -0500

    brain calculator added

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,48 @@
+import random
+import operator
+
+
+def run_game():
+    print("Welcome to the Brain Games!")
+    print("May I have your name?")
+    name = input()
+    print(f"Hello, {name}!")
+
+    print("What is the result of the expression?")
+
+    rounds_count = 0
+    while rounds_count < 3:
+        number1 = random.randint(1, 100)
+        number2 = random.randint(1, 100)
+        simbol = random.choice(["+", "-", "*"])
+        expression = f"{number1} {simbol} {number2}"
+        print(f"Question: {expression}")
+        answer = input("Your answer: ")
+
+        operations = {
+            "+": operator.add,
+            "-": operator.sub,
+            "*": operator.mul,
+        }
+
+        expected_answer = operations[simbol](number1, number2)
+
+        if answer == str(expected_answer):
+            print("Correct!")
+            rounds_count += 1
+        else:
+            print(
+                f"'{answer}' is wrong answer ;(. Correct answer was '{expected_answer}'."
+            )
+            print(f"Let's try again, {name}!")
+            return
+
+    print(f"Congratulations, {name}!")
+
+
+def main():
+    run_game()
+
+
+if __name__ == "__main__":
+    main()
