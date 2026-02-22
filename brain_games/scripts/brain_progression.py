9bf59f1da453ccb144d179b396fd3b7e07504f8b
alexlealch <alexlealch@gmail.com>
2026-02-22 12:23:08 -0500

    inicio cuarto juego

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,13 @@
+import random
+import math
+
+
+def radom_sequence():
+    return random.randint(1, 10)
+
+def run_game():
+    print("What number is missing in the progression?")
+
+    rounds_count = 0
+    while rounds_count < 3:
+        
