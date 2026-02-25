e02bf8250a11504a61a2a5187f637aa198095e66
alexlealch <alexlealch@gmail.com>
2026-02-25 15:35:54 -0500

    cambio extremo brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,32 +1,8 @@
 from brain_games.cli import welcome_user
-from brain_games.scripts.brain_even import run_game as brain_even
-from brain_games.scripts.brain_calc import run_game as brain_calc
-from brain_games.scripts.brain_gcd import run_game as brain_gcd
-from brain_games.scripts.brain_progression import run_game as brain_progression
-from brain_games.scripts.brain_prime import run_game as brain_prime
 
 
 def main():
     welcome_user()
-    print("Which game would you like to play?")
-    print("1. Even or Odd")
-    print("2. Calculator")
-    print("3. GCD")
-    print("4. Progression")
-    print("5. Prime")
-    choice = input("Your choice: ")
-    if choice == "1":
-        brain_even()
-    elif choice == "2":
-        brain_calc()
-    elif choice == "3":
-        brain_gcd()
-    elif choice == "4":
-        brain_progression()
-    elif choice == "5":
-        brain_prime()
-    else:
-        print("Invalid choice")
 
 
 if __name__ == "__main__":
